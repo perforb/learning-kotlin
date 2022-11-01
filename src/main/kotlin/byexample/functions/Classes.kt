3dd3933fdc3953b2f198e432a843b9ba4d2516dd
perforb <dev.perfumed.garden@gmail.com>
2022-11-01 21:28:48 +0900

    Add Classes.kt

---
+++ src/main/kotlin/byexample/functions/Classes.kt
@@ -0,0 +1,18 @@
+package byexample.functions
+
+class Customer
+
+class Contact(val id: Int, var email: String) {
+  override fun toString(): String {
+    return "Contact(id=$id, email='$email')"
+  }
+}
+
+fun main() {
+  val customer = Customer()
+  val contact = Contact(1, "mary@gmail.com")
+
+  println(contact.id)
+  contact.email = "jane@gmail.com"
+  println(contact)
+}
