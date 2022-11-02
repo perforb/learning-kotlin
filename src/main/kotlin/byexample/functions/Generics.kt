2364745eae7b08b5d80c8c67b605d257d915761c
perforb <dev.perfumed.garden@gmail.com>
2022-11-02 21:18:45 +0900

    Add Generics.kt

---
+++ src/main/kotlin/byexample/functions/Generics.kt
@@ -0,0 +1,37 @@
+package byexample.functions
+
+class MutableStack<E>(vararg items: E) {
+
+  private val elements = items.toMutableList()
+
+  fun push(element: E) = elements.add(element)
+
+  fun peek(): E = elements.last()
+
+  fun pop(): E = elements.removeAt(elements.size - 1)
+
+  fun isEmpty() = elements.isEmpty()
+
+  fun size() = elements.size
+
+  override fun toString() = "MutableStack(${elements.joinToString()})"
+}
+
+fun <E> mutableStackOf(vararg elements: E) = MutableStack(*elements)
+
+fun main() {
+  val stack = MutableStack(0.62, 3.14, 2.7)
+  stack.push(9.87)
+  println(stack)
+
+  println("peek(): ${stack.peek()}")
+  println(stack)
+
+  for (i in 1..stack.size()) {
+    println("pop(): ${stack.pop()}")
+    println(stack)
+  }
+
+  val mutableStack = mutableStackOf(0.11, 3.14, 3.8)
+  println(mutableStack)
+}
